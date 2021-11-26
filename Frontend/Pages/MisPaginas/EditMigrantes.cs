7fcd6edf928caff4a6e2e17b98a392320607dc73
Annfel12 <86271102+Annfel12@users.noreply.github.com>
2021-11-26 18:06:54 -0500

    avances2 andrés

---
+++ Frontend/Pages/MisPaginas/EditMigrantes.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using Dominio.Entidades;
+using Persistencia.AppRepositorios;
+
+namespace Frontend.Pages
+{
+    public class EditMigrantesModel : PageModel
+    {
+ private readonly IRepositorioMigracion repositorioMigracion;
+        [BindProperty]
+
+        public Migrantes Migrantes {set; get;}
+
+        public EditMigrantesModel ()
+        {
+            this.repositorioMigracion = new RepositorioMigracion(new Persistencia.AppContext());
+        }
+        public IActionResult OnGet(int? migrantesId)
+        {
+            if (migrantesId.HasValue)
+            {
+                Migrantes = repositorioMigracion.GetMigrantes(migrantesId.Value);
+            }
+            else 
+            {
+                Migrantes = new Migrantes();
+            }
+            if (Migrantes == null)
+            {
+                return RedirectToPage("./Notfound");                
+            }
+            else
+            {
+                return Page();
+            }
+
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (Migrantes.Id>0)
+            {
+                Migrantes= repositorioMigracion.UpdateMigrantes(Migrantes);
+            }
+            else
+            {
+                repositorioMigracion.AddMigrantes(Migrantes);
+            }
+            return Page();
+        }        
+        
+    }
+}
--- Frontend/Pages/MisPaginas/Migrantes.cshtml.cs
+++ Frontend/Pages/MisPaginas/Migrantes.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dominio.Entidades;
+using Persistencia.AppRepositorios;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Frontend.Pages
 {
     public class MigrantesModel : PageModel
     {
-        public void OnGet()
+
+        private readonly IRepositorioMigracion repositorioMigracion;
+
+        public IEnumerable<Migrantes> Migrantes {get; set;}
+        public MigrantesModel()
         {
+            this.repositorioMigracion = new RepositorioMigracion(new Persistencia.AppContext());            
         }
+
+        public void OnGet(string filtroBusqueda)     
+        {
+            Migrantes = repositorioMigracion.GetAllMigrantes();
+        }  
     }
 }
