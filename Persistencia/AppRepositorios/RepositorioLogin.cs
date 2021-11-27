55098a5b8058c6ac647da4145863416d75aae1ba
xtraix20 <90512567+xtraix20@users.noreply.github.com>
2021-11-27 11:41:51 -0500

    necesidad

--- Persistencia/AppRepositorios/RepositorioLogin.cs
+++ Persistencia/AppRepositorios/RepositorioLogin.cs
@@ -8,21 +8,6 @@ namespace Persistencia.AppRepositorios
 {
     public class RepositorioLogin : IRepositorioLoginClave
     {
-<<<<<<< HEAD
-        List<ListaLogin> Migrantes = new List<ListaLogin>()
-            {
-                new ListaLogin{numero_documento=" ", contraseña=" "},
-                
-            };     
-     
-        public bool GetLogin(string numero_documento, string contraseña)
-        {
-          foreach (ListaLogin adress in Migrantes)
-          {
-
-              if (adress.numero_documento == numero_documento){
-                  if (adress.contraseña == contraseña){
-=======
         List<ListaLogin> listado3 = new List<ListaLogin>()
             {
                 new ListaLogin{User=" ", Pass=" "},
@@ -36,7 +21,6 @@ namespace Persistencia.AppRepositorios
 
               if (adress.User == user){
                   if (adress.Pass == pass){
->>>>>>> acc5bf4c0f0bf9323b381faf1620d392f9019842
                       return true;
                   }
               }              
